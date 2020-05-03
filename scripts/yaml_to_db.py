00a21decb17dd8fab815da2a50ff4242e3642fbc
Alexander Fleischer <fleisch91@gmail.com>
2020-05-04 01:43:58 +0200

    Update script

--- scripts/yaml_to_db.py
+++ scripts/yaml_to_db.py
@@ -9,24 +9,29 @@ from bot_api.models import Event
 
 
 database_url = os.environ.get("DATABASE_URL")
-schedules_filepath = "../res/schedules.yaml"
+if len(sys.argv) > 1:
+    schedules_filepath = sys.argv[1]
+else:
+    sys.exit("Usage: python yaml_to_db.py /path/to/schedules.yaml")
+
 table_name = "schedule"
 
-engine = create_engine(database_url, echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
+if __name__ == "__main__":
+    engine = create_engine(database_url, echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-conn = engine.connect()
+    conn = engine.connect()
 
-with open(schedules_filepath, "rb") as fs:
-    schedule_dict = yaml.safe_load(fs)
+    with open(schedules_filepath, "rb") as fs:
+        schedule_dict = yaml.safe_load(fs)
 
-for x in session.query(Event).all():
-    print(x.when, x.event_type, x.what, x.who)
+    for x in session.query(Event).all():
+        print(x.when, x.event_type, x.what, x.who)
 
-values = [
-    Event(event_type=vals["event"], when=date, what=vals["what"], who=vals["who"]) 
-        for date, vals in schedule_dict.items()]
+    values = [
+        Event(event_type=vals["event"], when=date, what=vals["what"], who=vals["who"]) 
+            for date, vals in schedule_dict.items()]
 
-session.add_all(values)
-session.commit()
+    session.add_all(values)
+    session.commit()
