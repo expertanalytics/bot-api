241ffd6e2bfffe6aa3108d8631260f7b86c23ca8
Alexander Fleischer <fleisch91@gmail.com>
2020-05-03 11:01:39 +0200

    Update schedule function

--- bot_api/main.py
+++ bot_api/main.py
@@ -46,41 +46,97 @@ async def events(request: Request):
 
 
 @app.post("/api/v1.0/schedule")
-async def schedule(*, channel_id: str = Form(...)):
+async def schedule(channel_id: str = Form(...)):
 
-    response = {
-            "response_type": "in_channel",
-            "text": "Scheduling is not yet setup."
-            }
+    args = models.get_args_from_request(text)
+
+    if not args.who or not args.what or not args.what:
+        return {
+                "text": models.default_responses["USAGE_ERROR"],
+                "response_type": "ephemeral"}
+
+    when = dateparser.parse(args.when) 
+    if not when:
+        return models.default_responses["DATE_ERROR"]
+
    
-    return response
+    return "Scheduling is not yet setup."
 
+
+
+@app.post("/api/v1.0/add")
+async def create_event(text: str = Form(...), db: Session = Depends(get_db)):
+
+    args = models.get_args_from_request(text)
+
+    when = dateparser.parse(
+            args.when, 
+            settings={'STRICT_PARSING': True})
+
+
+    if args.event not in models.event_types:
+        return models.default_responses["MISSING_EVENT_ERROR"]
+
+    if not when:
+        return models.default_responses["DATE_ERROR"]
+
+    when = when.date()
+    if datetime.datetime.now().date() > when:
+        return models.default_responses["PAST_DATE_ERROR"]
+    
+    db_event = crud.get_event_by_date(db, when=when)
+    if db_event:
+        return models.default_responses["EXISTING_DATE_ERROR"]
+
+    crud.create_event(
+            db=db, event_type=args.event, when=when)
+
+    return (f"{models.prettify_date(when)}"
+            f" successfully added to the schedule.")
+
+
+@app.post("/api/v1.0/remove")
+async def delete_event(
+        text: str = Form(...), 
+        db: Session = Depends(get_db)):
+
+    args = models.get_args_from_request(text)
+    when = dateparser.parse(
+            args.when, 
+            settings={'STRICT_PARSING': True})
+
+    if not when:
+        return "Error: Unable to parse date."
+
+    when = when.date()
+    if datetime.datetime.now().date() > when:
+        return "Error: Date is in the past."
+    
+    db_event = crud.get_event_by_date(db, when=when)
+    if not db_event:
+        return "Error: Date not in schedule.",
+
+    crud.remove_event(
+            db=db, when=when)
+
+    return (f"{models.prettify_date(when)} "
+            f"successfully removed from the schedule.")
 
 
 @app.post("/api/v1.0/next")
 async def read_event(db: Session = Depends(get_db)):
     db_event = crud.get_closest_event(db, when=date.today())
     if db_event is None or not db_event.who:
-        return JSONResponse({
-            "text": "No upcoming events.",
-            "response_type": "ephemeral"})
+        return models.default_responses["NO_EVENTS"]
 
-    response = models.get_formatted_event(db_event)
-    return JSONResponse({
-        "text": response,
-        "response_type": "in_channel"})
+    return models.get_formatted_event(db_event)
 
 
 @app.post("/api/v1.0/upcoming")
 async def read_events(db: Session = Depends(get_db)):
     db_events = crud.get_upcoming_events(db, when=date.today())
     if db_events is None:
-        return JSONResponse({
-            "text": "No upcoming events.",
-            "response_type": "ephemeral"})
+        return models.default_responses["NO_EVENTS"]
 
-    response = "\n".join(
+    return "\n".join(
             [models.get_formatted_event(event) for event in db_events])
-    return JSONResponse({
-        "text": response,
-        "response_type": "in_channel"})
