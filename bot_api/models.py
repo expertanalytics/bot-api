ad0d2ebbeeff1b2dcb0799f1a4490637c53faa21
Alexander Fleischer <fleisch91@gmail.com>
2020-05-03 11:01:52 +0200

    Add argument parser and dicts

--- bot_api/models.py
+++ bot_api/models.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+
 from sqlalchemy import (Column, Integer, String, Date)
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -33,3 +35,27 @@ def get_formatted_event(event: Event):
 
     fagdag_tag = f" :busts_in_silhouette: Fagdag" if is_fagdag else ""
     return f">{response}{fagdag_tag}"
+
+
+def get_args_from_request(request):
+    """Creates an ArgumentParser and parses input arguments"""
+    cmd_parser = ArgumentParser()
+
+    cmd_parser.add_argument("--who", type=str)
+    cmd_parser.add_argument("--what", type=str)
+    cmd_parser.add_argument("--when", type=str)
+    cmd_parser.add_argument("--event", type=str)
+
+    return cmd_parser.parse_args(request.split())
+
+
+default_responses = {
+        "DATE_ERROR": "Error: Unable to parse date.",
+        "MISSING_EVENT_ERROR": "Error: No event specified.",
+        "EXISTING_DATE_ERROR": "Error: Date already in schedule",
+        "PAST_DATE_ERROR": "Error: Date is in the past.",
+        "NO_EVENTS": "No upcoming events.",
+        "USAGE_ERROR": "Incorrect use of command."
+        }
+
+event_types = ["fagdag", "formiddag"]
