ade7c66d47ed64c3349b1fd292ced6d2e8ee65fa
Alexander Fleischer <fleisch91@gmail.com>
2020-05-03 11:01:01 +0200

    Fix crud funcs

--- bot_api/crud.py
+++ bot_api/crud.py
@@ -5,11 +5,18 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
-def create_event(db: Session, events: schemas.EventCreate, when: date):
-    db_event = models.Event(**sched.dict(), when=when)
+def create_event(db: Session, when: date, event_type: str):
+    db_event = models.Event(when=when, event_type=event_type)
     db.add(db_event)
     db.commit()
     db.refresh(db_event)
+    return db_event
+
+
+def remove_event(db: Session, when: date):
+    db_event = db.query(models.Event).filter(models.Event.when == when).first()
+    db.delete(db_event)
+    db.commit()
     return db_event
 
 
