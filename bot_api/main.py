8f073bc801a493617543f1a8a6f2bb4e960823cf
Alexander Fleischer <fleisch91@gmail.com>
2020-05-12 16:58:56 +0200

    Fix typo

--- bot_api/main.py
+++ bot_api/main.py
@@ -47,7 +47,7 @@ def get_db():
 def validate_request(request):
     request_body = request.body()
     timestamp = request.headers['X-Slack-Request-Timestamp']
-    if absolute_value(time.time() - timestamp) > 60 * 5:
+    if abs(time.time() - timestamp) > 60 * 5:
         # The request timestamp is more than five minutes from local time.
         # It could be a replay attack, so let's ignore it.
         return False
