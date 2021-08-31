01426b682447151d993da5c861f41f06d9f9738f
Alexander Fleischer <fleisch91@gmail.com>
2021-08-31 16:04:47 +0200

    Clean up code pt 2

--- bot_api/errors.py
+++ bot_api/errors.py
@@ -1,29 +1,38 @@
 class UsageError(Exception):
     pass
 
+
 class InvalidEventError(Exception):
     pass
+
 
 class InvalidDateError(Exception):
     pass
 
+
 class PastDateError(Exception):
     pass
+
 
 class MissingDateError(Exception):
     pass
 
+
 class AlreadyScheduledError(Exception):
     pass
+
 
 class AlreadyClearedError(Exception):
     pass
 
+
 class AlreadyCancelledError(Exception):
     pass
 
+
 class ExistingDateError(Exception):
     pass
+
 
 class ArgumentError(Exception):
     pass
